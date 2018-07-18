10f28485eb920015e2e5df21abe469c76f9e1267
Beckmann, Kai <kai.beckmann@hs-rm.de>
2018-07-18 12:27:28 +0000

    update rail driver

--- drivers/include/rail_drv.h
+++ drivers/include/rail_drv.h
@@ -20,8 +20,6 @@
 #include "net/gnrc/nettype.h"
 #include "net/eui64.h"
 
-// silabs includes
-
 #include "rail.h"
 
 #ifdef __cplusplus
@@ -29,8 +27,30 @@ extern "C" {
 #endif
 
 
-// states of the radio transceiver
+/**
+ * @name    Channel configuration
+ * @{
+ */
+#define RAIL_868MHZ_MIN_CHANNEL           (IEEE802154_CHANNEL_MIN_SUBGHZ)
+#define RAIL_868MHZ_MAX_CHANNEL           (IEEE802154_CHANNEL_MIN_SUBGHZ) /* 868MHz has only ONE channel! */
+#define RAIL_868MHZ_DEFAULT_CHANNEL       (IEEE802154_CHANNEL_MIN_SUBGHZ)
+#define RAIL_912MHZ_MIN_CHANNEL           (IEEE802154_CHANNEL_MIN_SUBGHZ)
+#define RAIL_912MHZ_MAX_CHANNEL           (IEEE802154_CHANNEL_MAX_SUBGHZ)
+#define RAIL_912MHZ_DEFAULT_CHANNEL       (IEEE802154_DEFAULT_SUBGHZ_CHANNEL)
+/* Page 2 is O-QPSK 100 kbit/s (channel 0), or 250 kbit/s (channels 1-10) */
+#define RAIL_SUBGHZ_DEFAULT_PAGE          (IEEE802154_DEFAULT_SUBGHZ_PAGE)
+#define RAIL_2P4GH_MIN_CHANNEL           (IEEE802154_CHANNEL_MIN)
+#define RAIL_2P4GH_MAX_CHANNEL           (IEEE802154_CHANNEL_MAX)
+#define RAIL_2P4GH_DEFAULT_CHANNEL       (IEEE802154_DEFAULT_CHANNEL)
+/* Only page 0 is supported in the 2.4 GHz band */
 
+/** @} */
+
+/* states of the radio transceiver */
+/* TODO
+    - use enum?
+    - use !
+*/
 #define RAIL_TRANSCEIVER_STATE_UNINITIALIZED    0x00
 #define RAIL_TRANSCEIVER_STATE_IDLE             0x01
 #define RAIL_TRANSCEIVER_STATE_SLEEP            0x02
@@ -39,48 +59,39 @@ extern "C" {
 #define RAIL_TRANSCEIVER_STATE_TX               0x05
 
 
-//#define RAIL_DEFAULT_PANID         (IEEE802154_DEFAULT_PANID)
-#define RAIL_DEFAULT_PANID         0x0023
+#define RAIL_DEFAULT_PANID         (IEEE802154_DEFAULT_PANID)
 #define RAIL_DEFAULT_TXPOWER       (IEEE802154_DEFAULT_TXPOWER)
+#define RAIL_DEFAULT_CSMA_TRIES     5
+
+enum rail_transceiver_config_frequency {
+    RAIL_TRANSCEIVER_FREQUENCY_2P4GHZ,
+    RAIL_TRANSCEIVER_FREQUENCY_868MHZ,
+    RAIL_TRANSCEIVER_FREQUENCY_912MHZ
+};
 
 
 typedef struct rail_params {
+    enum rail_transceiver_config_frequency freq;
 
-    int tmp;
-    uint64_t XTAL_frequency;
-    uint64_t BASE_frequency;
-    int32_t max_transit_power;
 } rail_params_t;
 
 typedef struct {
     netdev_ieee802154_t netdev;
     rail_params_t params;
 
-    // handle of the rail blob driver instance
+    /* handle of the RAIL driver blob instance */
     RAIL_Handle_t rhandle;
-    // config of the rail blob driver
+    /* config of the RAIL driver blob */
     RAIL_Config_t rconfig;
+    /* config for CSMA */
+    RAIL_CsmaConfig_t csma_config;
 
-    uint8_t state;              // state of radio
-
-    // state waiting for ack
-    volatile bool waiting_for_ack;
+    uint8_t state;              /* state of radio transceiver */
 
     RAIL_RxPacketHandle_t lastRxPacketHandle;
 
-
     bool promiscuousMode;
     eui64_t eui;
-
-    uint8_t csmaTries;   // number of csma retries
-    bool macNoSleep;
-
-    // tmp receive buffer till proper buffer management is avaiable
-    uint8_t recv_frame[IEEE802154_FRAME_LEN_MAX + 1];
-    bool recv_taken;
-    uint16_t recv_size;
-    uint8_t recv_rssi;
-    uint8_t recv_lqi;
 } rail_t;
 
 
@@ -88,7 +99,6 @@ void rail_setup(rail_t *dev, const rail_params_t *params);
 
 int rail_init(rail_t *dev);
 
-int rail_tx_prepare(rail_t *dev);
 
 int rail_start_rx(rail_t *dev);
 
